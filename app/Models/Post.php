775b07b68dec853c91c37eaba2c3d5aa1b0e8f7f
sultonialwi64 <sultonialwi64@gmail.com>
2026-01-19 15:59:51 +0700

    feat: implement all restful requirements and policies

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,10 +13,13 @@ class Post extends Model
     protected $fillable = ['title', 'content', 'is_draft', 'published_at', 'user_id'];
 
     public function scopeActive($query)
-    {
-        return $query->where('is_draft', false)
-                     ->where('published_at', '<=', now()); //
-    }
+{
+    return $query->where('is_draft', false)
+                 ->where(function ($q) {
+                     $q->whereNull('published_at')
+                       ->orWhere('published_at', '<=', now());
+                 });
+}
 
     public function user()
     {
